17f4d8132db2f202347b573ffcd686d491c64541
xiangyun <xiangyunfaith@outlook.com>
2022-01-02 17:25:17 +0800

    添加如何使用中英文字体的说明

--- _common.R
+++ _common.R
@@ -6,3 +6,32 @@ knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE
   )
+
+# 准备 Noto 中英文字体
+# sysfonts::font_paths(new = "~/Library/Fonts/")
+## 宋体
+sysfonts::font_add(
+  family = "Noto Serif CJK SC",
+  regular = "NotoSerifCJKsc-Regular.otf",
+  bold = "NotoSerifCJKsc-Bold.otf"
+)
+## 黑体
+sysfonts::font_add(
+  family = "Noto Sans CJK SC",
+  regular = "NotoSansCJKsc-Regular.otf",
+  bold = "NotoSansCJKsc-Bold.otf"
+)
+sysfonts::font_add(
+  family = "Noto Serif",
+  regular = "NotoSerif-Regular.ttf",
+  bold = "NotoSerif-Bold.ttf",
+  italic = "NotoSerif-Italic.ttf",
+  bolditalic = "NotoSerif-BoldItalic.ttf"
+)
+sysfonts::font_add(
+  family = "Noto Sans",
+  regular = "NotoSans-Regular.ttf",
+  bold = "NotoSans-Bold.ttf",
+  italic = "NotoSans-Italic.ttf",
+  bolditalic = "NotoSans-BoldItalic.ttf"
+)
